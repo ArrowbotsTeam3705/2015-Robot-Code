795ef3e16a4cd1b9b399c2bb49e609b3e4984a94
Javed Nissar <javednissar@gmail.com>
2015-02-17 15:23:18 -0500

    added relay control

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -13,6 +13,9 @@ using std::string;
  * don't. Unless you know what you are doing, complex code will be much more difficult under
  * this system. Use IterativeRobot or Command-Based instead if you're new.
  */
+//Author Name:Javed Nissar
+//Purpose:Classified
+//Date:February 13,2070
 /*
                                    .'########:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: ,## `#######
                                 ;############+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::, ##` #######.
@@ -96,19 +99,19 @@ class Robot: public SampleRobot
 	//two limit switches that act as endpoints for the robot
 	DigitalInput topSwitch;
 	DigitalInput bottomSwitch;
-	DigitalInput middleSwitch;
 	//counters measuring the number of times the limit switch has been hit
 	Counter topCounter;
 	Counter bottomCounter;
-	Counter middleCounter;
-	DigitalOutput spotIndicator;
+	Encoder encoder;
 	Joystick forkLiftControl;
+	Relay indicator;
+	//Jawad was here
 	//amount of time that must occur between inputs for pneumatic
 	double secondsForPneumatic;
 	//boolean controlling whether or not robot is inverted
 	bool inverted;
-	int previousMiddleCount;
-	int middleCount;
+	bool indicatorTest;
+	bool doesRobotHaveTote;
 public:
 	Robot() :
 			myRobot(0, 1),	// these must be initialized in the same order
@@ -116,20 +119,18 @@ public:
 			forkLift(2),
 			leftHook(0),
 			rightHook(1),
-			topSwitch(1),
+			topSwitch(2),
 			bottomSwitch(0),
-			middleSwitch(2),
 			//limit switches must be passed to counter as pointers
 			topCounter(&topSwitch),
 			bottomCounter(&bottomSwitch),
-			middleCounter(&middleSwitch),
-			spotIndicator(3),
-			forkLiftControl(1)// as they are declared above.
+			encoder(7,8),
+			forkLiftControl(1),// as they are declared above.
+			indicator(0)
 	{
 		myRobot.SetExpiration(0.1);
 		inverted=false;
 		secondsForPneumatic=0.5;
-		previousMiddleCount=0;
 	}
 
 	/**
@@ -167,7 +168,23 @@ public:
 		myRobot.SetSafetyEnabled(true);
 		while (IsOperatorControl() && IsEnabled())
 		{
-			middleCount=middleCounter.Get();
+			//tell the robot whether or not you have a tote when you press button 3 on the joystick
+			if(forkLiftControl.GetRawButton(3)){
+				if(doesRobotHaveTote){
+					doesRobotHaveTote=false;
+					indicator.Set(Relay::kOff);
+					//reset the counter and make it stop updating
+					topCounter.Reset();
+				}else{
+					//tell the counter to start updating again
+					doesRobotHaveTote=true;
+					topCounter.SetUpdateWhenEmpty(false);
+				}
+			}
+			//if the robot has a tote and the middle counter has been hit, then flash the indicator
+			if(doesRobotHaveTote&&(topCounter.Get()>0)){
+				indicator.Set(Relay::kForward);
+			}
 			//if inverted, then invert drive and if not inverted, then don't
 			if(inverted){
 				myRobot.ArcadeDrive(controller.GetY(),controller.GetZ());
@@ -186,32 +203,35 @@ public:
 			 *the motor of the pulley will be at 75% forwards. The top switch must not be triggered in order to prevent any damage from being done to the robot.
 			 *the need for it to be above 0.1 is to accommodate the drift of the right stick of the controller (joystick value is never 0)
 			 */
-			if((forkLiftControl.GetY()>0.1)&&(topCounter.Get()==0)){
-				forkLift.Set(forkLiftControl.GetY());
+			double forkLiftControlY=forkLiftControl.GetY();
+			if((forkLiftControlY>0.1)&&(topCounter.Get()==0)){
+				forkLift.Set(forkLiftControlY);
 				bottomCounter.Reset();
 			}
 			/*
 			 * when you move right stick of controller downwards and the bottom switch has not been triggered, the pulley will move downwards
 			 * the motor of the pulley will be at 50% reverse. The bottom switch must not be triggered in order to prevent any damage from being done to the robot.
 			 */
-			else if((forkLiftControl.GetY()<-0.1)&&(bottomCounter.Get()==0)){
-				forkLift.Set(forkLiftControl.GetY());
+			else if((forkLiftControlY<-0.1)&&(bottomCounter.Get()==0)){
+				forkLift.Set(forkLiftControlY);
 				topCounter.Reset();
 			}
-
 			//when right stick of controller is left alone, motor will not exert force on pulley; thus, causing the pulley to drift downwards.
 			else{
 				stopPulley();
 			}
+
 			SmartDashboard::PutNumber("Top switch",topCounter.Get());
-			SmartDashboard::PutNumber("Middle count",middleCount);
-			SmartDashboard::PutNumber("Previous middle count",previousMiddleCount);
-			SmartDashboard::PutNumber("Bottom switch",bottomCounter.Get());
+			//SmartDashboard::PutBoolean("Does robot have a tote?",doesRobotHaveTote);
+			SmartDashboard::PutBoolean("Indicator",doesRobotHaveTote&&(topCounter.Get()>0));
+			SmartDashboard::PutNumber("encoder",encoder.Get());
 
 			//provide status on whether or not hooks can be moved
 			SmartDashboard::PutString("Can I press right trigger to move hooks",CanHooksBeMoved());
 			//provide status on whether or not controls are inverted
 			SmartDashboard::PutBoolean("inverted",inverted);
+			//tells you if a tote is currently on the robot
+			SmartDashboard::PutBoolean("Does robot have tote?",doesRobotHaveTote);
 			//if button 8 on the controller is pressed (the right trigger on Maninder's red controller) and the timer on the pneumatic is not active
 			//this is meant to prevent the hooks from bouncing by ensuring that the button input is not taken at all times
 			if(controller.GetRawButton(8)&&!(pneumaticTimer.Get()>0)){
@@ -228,11 +248,7 @@ public:
 				pneumaticTimer.Stop();
 				pneumaticTimer.Reset();
 			}
-			if(middleCount>previousMiddleCount){
-				spotIndicator.Pulse(0.0016);
-			}
-			previousMiddleCount=middleCount;
-			//Indicators when the forklift is at its "soft spot" (Not Sure which LimitSwitch)
+
 			Wait(0.005);			// wait for motor update time
 		}
 	}
@@ -246,3 +262,4 @@ public:
 };
 
 START_ROBOT_CLASS(Robot);
+
